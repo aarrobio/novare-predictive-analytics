aee7d0858e8504a098cb70887b0a9947fd82bc52
aarrobio <anne.arrobio@novare.com.hk>
2021-06-23 17:16:19 +0800

    Updated UI
    updated UI

--- app.py
+++ app.py
@@ -3,32 +3,18 @@ import pandas as pd
 import streamlit as st
 from sklearn.preprocessing import StandardScaler
 import pickle
-
 from PIL import Image
 
-st.markdown('# Predict if tumor is benign or malignant')
+st.set_page_config(page_title='Know Your Tumor State',layout="wide")
+
+col1, col2, col3, col4 = st.beta_columns((1,2,3,1))
+
 icon = Image.open("images/bandana.png")
-st.image(icon)
+col2.image(icon, use_column_width=True)
+col3.title('Is your tumor benign or malignant?')
+col3.write('This application is created to help breast cancer patients and health care professionals to gain information on the tumor state using a machine learning model. It specifically uses the support vector machines algorithm to classify tumor state. Input the important metrics below to know whether the tumor is benign or malignant.')
 
-
-st.title('This is a title')
-st.header('Header')
-st.subheader('Sub Header')
-st.write('Can display many things')
-
-#import time
-#my_bar = st.progress(0)
-#for percent_complete in range(100):
-#    time.sleep(0.1)
-#    my_bar.progress(percent_complete + 1)
-#st.spinner()
-#with st.spinner(text='In progress'):
-#    time.sleep(5)
-#    st.success('Done')
-#e = RuntimeError('This is an exception of type RuntimeError')
-#st.exception(e)
-
-st.header('Header')
+st.header('Input metrics below:')
 
 def predict_tumor(features):
     model = pickle.load(open('svc_fin.pkl', 'rb'))
@@ -45,35 +31,37 @@ def predict_tumor(features):
 
     output = prediction[0]
 
-    result = f"Tumor is {output}."
+    result = f"Your tumor is {output}."
     return result
 
 features = []
 
-features.append(st.number_input('Mean Radius',format="%.4f"))
-features.append(st.number_input('Mean Texture',format="%.4f"))
-features.append(st.number_input('Mean Perimeter',format="%.4f"))
-features.append(st.number_input('Mean Area',format="%.4f"))
-features.append(st.number_input('Mean Compactness',format="%.4f"))
-features.append(st.number_input('Mean Concavity',format="%.4f"))
-features.append(st.number_input('Mean Concave Points',format="%.4f"))
-features.append(st.number_input('Radius Error',format="%.4f"))
-features.append(st.number_input('Perimeter Error',format="%.4f"))
-features.append(st.number_input('Area Error',format="%.4f"))
-features.append(st.number_input('Concave Points Error',format="%.4f"))
-features.append(st.number_input('Worst Radius',format="%.4f"))
-features.append(st.number_input('Worst Texture',format="%.4f"))
-features.append(st.number_input('Worst Perimeter',format="%.4f"))
-features.append(st.number_input('Worst Area',format="%.4f"))
-features.append(st.number_input('Worst Smoothness',format="%.4f"))
-features.append(st.number_input('Worst Compactness',format="%.4f"))
-features.append(st.number_input('Worst Concavity',format="%.4f"))
-features.append(st.number_input('Worst Concave Points',format="%.4f"))
-features.append(st.number_input('Worst Symmetry',format="%.4f"))
+c1, c2, c3, c4 = st.beta_columns(4)
+features.append(c1.number_input('Mean Radius',format="%.4f",value=17.99))
+features.append(c1.number_input('Mean Texture',format="%.4f",value=10.38))
+features.append(c1.number_input('Mean Perimeter',format="%.4f",value=122.80))
+features.append(c1.number_input('Mean Area',format="%.4f",value=1001.0))
+features.append(c1.number_input('Mean Compactness',format="%.4f",value=0.27760))
+
+features.append(c2.number_input('Mean Concavity',format="%.4f",value=0.30010))
+features.append(c2.number_input('Mean Concave Points',format="%.4f",value=0.14710))
+features.append(c2.number_input('Radius Error',format="%.4f",value=1.0950))
+features.append(c2.number_input('Perimeter Error',format="%.4f",value=8.589))
+features.append(c2.number_input('Area Error',format="%.4f",value=153.40))
+
+features.append(c3.number_input('Concave Points Error',format="%.4f",value=0.01587))
+features.append(c3.number_input('Worst Radius',format="%.4f",value=25.38))
+features.append(c3.number_input('Worst Texture',format="%.4f",value=17.33))
+features.append(c3.number_input('Worst Perimeter',format="%.4f",value=184.60))
+features.append(c3.number_input('Worst Area',format="%.4f",value=2019.0	))
+
+features.append(c4.number_input('Worst Smoothness',format="%.4f",value=0.1622))
+features.append(c4.number_input('Worst Compactness',format="%.4f",value=0.6656))
+features.append(c4.number_input('Worst Concavity',format="%.4f",value=0.7119))
+features.append(c4.number_input('Worst Concave Points',format="%.4f",value=0.2654))
+features.append(c4.number_input('Worst Symmetry',format="%.4f",value=0.4601))
 
 if st.button('Predict'):
     predictions = predict_tumor(features)
     st.write(predictions)
-
-
 
