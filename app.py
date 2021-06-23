bb82e0a3860df823a53fe90cc9e501ca6273202e
aarrobio <anne.arrobio@novare.com.hk>
2021-06-23 15:47:00 +0800

    Create streamlit application
    Updated the pkl files and added app.py to deploy model using streamlit

---
+++ app.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+from sklearn.preprocessing import StandardScaler
+import pickle
+
+from PIL import Image
+
+st.markdown('# Predict if tumor is benign or malignant')
+icon = Image.open("images/bandana.png")
+st.image(icon)
+
+
+st.title('This is a title')
+st.header('Header')
+st.subheader('Sub Header')
+st.write('Can display many things')
+
+#import time
+#my_bar = st.progress(0)
+#for percent_complete in range(100):
+#    time.sleep(0.1)
+#    my_bar.progress(percent_complete + 1)
+#st.spinner()
+#with st.spinner(text='In progress'):
+#    time.sleep(5)
+#    st.success('Done')
+#e = RuntimeError('This is an exception of type RuntimeError')
+#st.exception(e)
+
+st.header('Header')
+
+def predict_tumor(features):
+    model = pickle.load(open('svc_fin.pkl', 'rb'))
+    trainset = pickle.load(open('training_data.pkl', 'rb'))
+    
+    newdata = pd.Series(features, index=trainset.columns)
+    trainset = trainset.append(newdata, ignore_index=True)
+
+    final_features = pd.DataFrame(StandardScaler().fit_transform(trainset), columns=trainset.columns)
+    final_features = final_features.iloc[-1:]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+    result = f"Tumor is {output}."
+    return result
+
+features = []
+
+features.append(st.number_input('Mean Radius',format="%.4f"))
+features.append(st.number_input('Mean Texture',format="%.4f"))
+features.append(st.number_input('Mean Perimeter',format="%.4f"))
+features.append(st.number_input('Mean Area',format="%.4f"))
+features.append(st.number_input('Mean Compactness',format="%.4f"))
+features.append(st.number_input('Mean Concavity',format="%.4f"))
+features.append(st.number_input('Mean Concave Points',format="%.4f"))
+features.append(st.number_input('Radius Error',format="%.4f"))
+features.append(st.number_input('Perimeter Error',format="%.4f"))
+features.append(st.number_input('Area Error',format="%.4f"))
+features.append(st.number_input('Concave Points Error',format="%.4f"))
+features.append(st.number_input('Worst Radius',format="%.4f"))
+features.append(st.number_input('Worst Texture',format="%.4f"))
+features.append(st.number_input('Worst Perimeter',format="%.4f"))
+features.append(st.number_input('Worst Area',format="%.4f"))
+features.append(st.number_input('Worst Smoothness',format="%.4f"))
+features.append(st.number_input('Worst Compactness',format="%.4f"))
+features.append(st.number_input('Worst Concavity',format="%.4f"))
+features.append(st.number_input('Worst Concave Points',format="%.4f"))
+features.append(st.number_input('Worst Symmetry',format="%.4f"))
+
+if st.button('Predict'):
+    predictions = predict_tumor(features)
+    st.write(predictions)
+
+
+
